a6b90c708d6bf943671995002d437e29db2dc41f
Joshua Taylor <joshuajtaylor1994@gmail.com>
2021-08-29 19:16:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
  # The configuration for the `remote` backend.
- terraform {
-       backend "remote" {
-         organization =  "aircall-sre-tech-test"
-         workspaces {
-           name = "aircall-sre-tech-test"
-         }
-       }
+terraform {
+   backend "remote" {
+   organization =  "aircall-sre-tech-test"
+     workspaces {
+       name = "aircall-sre-tech-test"
      }
+   }
+ }
